8bee627c4747a1a9fbc37ebc0065b0385eb011be
stavros-0 <sconsta@bu.edu>
2024-11-05 20:37:06 -0500

    More Styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,28 @@ const StyledDiv=styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  font-size:large;
+  text-decoration: none;
+  padding: 1%;
+  margin: 1%;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  &:hover {
+    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.3);
+    }
+`;
+
+const StyledInput = styled.input `
+  padding: 1%;
+  margin: 2%;
+  border: solid black 1px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  width: 200px;
+  font-size: 16px;
 `;
 
 export default function Home(){
@@ -18,8 +40,8 @@ export default function Home(){
     <StyledDiv>
       <h1>Find the Weather in any city!</h1>
       <p>Enter a city name below to get the current weather</p>
-      <input type="text" value={city} placeholder="City name" onChange={(e) => setCity(e.target.value)}/>
-      <Link href={`/${city}`}>Get Weather</Link>
+      <StyledInput type="text" value={city} placeholder="City name" onChange={(e) => setCity(e.target.value)}/>
+      <StyledLink href={`/${city}`}>Get Weather</StyledLink>
     </StyledDiv>
   );
 }
